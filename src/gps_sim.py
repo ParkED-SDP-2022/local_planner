fe2fec35f38f23408f9230a4b0c32f5a0b1472c4
MuizMohamad <muizzuddinmohamad@gmail.com>
2022-03-02 00:05:29 +0000

    Added: odom to gps convert to be use for turtlebot simulation

---
+++ src/gps_sim.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import math
+import rospy
+from parked_custom_msgs.msg import Point, Robot_Sensor_State
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+
+### node to simulate gps from turtlebot3 sim 
+
+class OdomToGPSConverter:
+
+    def __init__(self):
+
+        rospy.Subscriber("/odom",Odometry,self.subscribe)
+        self.gps_pub = rospy.Publisher('bench1/gps_pos',Point,queue_size=10)
+
+        rospy.spin()
+
+    def publish(self,point):
+        self.gps_pub.publish(point)
+
+
+    def subscribe(self,data):
+
+        self.odometry = data
+        
+        point = Point()
+        point.long = data.twist.twist.linear.x
+        point.lat = data.twist.twist.linear.y
+        point.angle = -999
+        
+        self.publish(point)
+        
+
+if __name__ == '__main__':
+    rospy.init_node("odom_gps_convert")
+    test = OdomToGPSConverter()
